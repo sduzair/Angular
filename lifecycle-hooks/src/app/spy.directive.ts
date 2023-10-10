5ce6b268c95cdadd602d3e0f4fd799fb627988d2
sduzair <sduzairahmed@gmail.com>
2023-10-10 14:16:50 -0400

    Spy directive

---
+++ lifecycle-hooks/src/app/spy.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, OnInit, OnDestroy } from '@angular/core';
+
+import { LoggerService } from './logger.service';
+
+// Spy on any element to which it is applied.
+// Usage: <div appSpy>...</div>
+@Directive({ selector: '[appSpy]' })
+export class SpyDirective implements OnInit, OnDestroy {
+  private static nextId = 1;
+  private id = SpyDirective.nextId++;
+
+  constructor(private logger: LoggerService) { }
+
+  ngOnInit() {
+    this.logger.log(`Spy #${this.id} onInit`);
+    console.log(`Spy #${this.id} onInit`);
+  }
+
+  ngOnDestroy() {
+    this.logger.log(`Spy #${this.id} onDestroy`);
+    console.log(`Spy #${this.id} onDestroy`);
+  }
+}
