0f01436de92142004085ea995954afbc68b33a72
sduzair <sduzairahmed@gmail.com>
2023-10-10 14:13:37 -0400

    Peek-a-boo all hooks

---
+++ lifecycle-hooks/src/app/peek-a-boo.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, OnInit } from '@angular/core';
+
+import { LoggerService } from './logger.service';
+
+let nextId = 0;
+
+@Directive({ selector: '[appPeekABoo]' })
+export class PeekABooDirective implements OnInit {
+  childLogs: string[];
+  constructor(private logger: LoggerService) {
+    this.childLogs = logger.logs;
+  }
+
+  // implement OnInit's `ngOnInit` method
+  ngOnInit() {
+    this.logIt('OnInit');
+  }
+
+  logIt(msg: string) {
+    this.logger.log(`#${++nextId} ${msg}`);
+    console.log(`#${nextId} ${msg}`);
+  }
+}
