06d0303afde9d7aeaba15a5f2b41c1469c9af80d
sduzair <sduzairahmed@gmail.com>
2023-10-11 14:13:27 -0400

    DoCheck Hook

---
+++ lifecycle-hooks/src/app/do-check-parent.component.ts
@@ -0,0 +1,28 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { DoCheckComponent } from './do-check.component';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'do-check-parent',
+  templateUrl: './do-check-parent.component.html'
+})
+export class DoCheckParentComponent {
+  hero!: Hero;
+  power = '';
+  title = 'DoCheck';
+  @ViewChild(DoCheckComponent) childView!: DoCheckComponent;
+
+  constructor() {
+    this.reset();
+    console.log('DoCheckParentComponent');
+  }
+
+  reset() {
+    this.hero = new Hero('Windstorm');
+    this.power = 'sing';
+    if (this.childView) {
+      this.childView.reset();
+    }
+  }
+}
