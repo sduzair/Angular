ea55bd69ae59e092b7332cf00e391c12e0436282
sduzair <sduzairahmed@gmail.com>
2024-01-05 05:13:09 -0500

    +dynamic form roles fetched asynchronously +dynamic form validation +form nested component +pipe +submit properly checks pending status

--- forms/src/app/user.service.ts
+++ forms/src/app/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable, delay, of } from "rxjs";
+import { Roles } from "./change-detection-reactive";
 
 @Injectable({
   providedIn: 'root',
@@ -8,5 +9,13 @@ export class UserService {
   isUserNameTaken(name: string): Observable<boolean> {
     // 'bob' is a taken
     return of(name === 'John').pipe(delay(2000));
+  }
+
+  getRoles(): Observable<Roles> {
+    return of({
+      admin: false,
+      editor: false,
+      subscriber: false
+    }).pipe(delay(2000));
   }
 }
